f45ed195c05fd0602bd3cb02e554cf711af938dc
cabo121 <armandocabo121@hotmail.com>
2021-03-14 16:07:20 -0600

    modelos

--- DbProyecto/posts/models.py
+++ DbProyecto/posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Post(models.Model):
 	personaje = models.TextField(default = "")
 	descripcion = models.TextField(default = "")
-	fecha = models.TextField(default = "")
+	superpoder = models.TextField(default = "")
 
 	def __str__ (self):
 		return self.personaje
